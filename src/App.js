1f141eea4473c8c6c6552de60b53b6656c63f17c
SARATH CHANDRAN M <sarathchandranm2001@gmail.com>
2024-11-04 11:57:36 +0530

    List mapping,key value pair

--- src/App.js
+++ src/App.js
@@ -2,22 +2,19 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const obtainedMark = 70;
-  const passMark = 60;
-
-  const ifPass = obtainedMark > passMark ? (<button>PassButton</button>) : (<button>FailButton</button>);
+  const data=[{name:'sarath',rollno:51,gender:'male'},
+    {name:'aswin',rollno:12,gender:'male'},
+    {name:'saleem',rollno:48,gender:'male'},
+    {name:'anshad',rollno:36,gender:'male'}]
 
   
   return (
     <div className="App">
-      <div>
-        {ifPass}
-      </div>
-      <header className="App-header">
-        <h1>Header 1</h1>
-        <h2>Header 2</h2>
-        <h3>Header 3</h3>
-      </header>
+      <h1>Names</h1>
+      <ul>
+        {data.map((items)=>
+        <li>{items.name}{items.rollno}</li>)}
+      </ul>
     </div>
   );
 }
