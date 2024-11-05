a2faa3c24f25e1a2734ea234ed85b304eb583d99
SARATH CHANDRAN M <sarathchandranm2001@gmail.com>
2024-11-05 10:23:55 +0530

    Props for items,description and price

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Amazonlogin1 from './Amazonlogin1';
+import Probs1 from './Probs1'
 
 
 
@@ -9,7 +10,10 @@ function App() {
   
   return (
    <div className="sign-up-container">
-    <Amazonlogin1/>
+    <h1>Product List</h1>
+    <Probs1 item="Laptop" price="$1000" description="A high-performance laptop" />
+    <Probs1 item="Mobile" price="$2000" description="Latest Smart Phone"/>
+    <Probs1 item="Headphone" price="$500" description="Best Quality Headphone"/>
     
     </div>
   );
---
+++ src/Probs1.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Probs1(props) {
+  return (
+    <div>
+        
+      <p>{props.item}, {props.price}, {props.description}</p>
+    </div>
+  );
+}
+
+export default Probs1;
